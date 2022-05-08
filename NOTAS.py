10c1f24780b116b96355f7c455ae216567a679e4
leticiagraziellegomes <102824656+leticiagraziellegomes@users.noreply.github.com>
2022-05-08 14:50:46 -0300

    Add files via upload

---
+++ NOTAS.py
@@ -0,0 +1,16 @@
+notas = [input("nota 1: ")
+,input("nota 2: ")
+,input("nota 3: ")
+,input("nota 4: ")]
+
+soma = float(notas[0]) + float(notas[1]) + float(notas[2]) + float(notas[3])
+
+media = soma / 4
+
+print("A media do aluno e " + str(media))
+
+
+
+
+
+
